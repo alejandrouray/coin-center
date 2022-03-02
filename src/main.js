febbec5cff38f98ae84d296d0798e9bcc9e0cfc0
Alejandro Uray <alejandrouray730@gmail.com>
2022-03-01 23:06:25 -0300

    Second commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from '@/router';
+import Chart from 'chart.js';
+import Chartick from 'vue-chartkick';
+import { VueSpinners } from '@saeris/vue-spinners';
 
-Vue.config.productionTip = false
+import { dollarFilter, percentFilter } from './filters';
+
+import '@/assets/css/tailwind.css';
+
+Vue.use(Chartick.use(Chart));
+Vue.use(VueSpinners);
+Vue.filter('dollar', dollarFilter);
+Vue.filter('percent', percentFilter);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
